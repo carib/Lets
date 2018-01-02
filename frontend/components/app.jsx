54324492c7f31f20f046e7f99e887077d72d11ad
Carib Rivera-Guerra <caribfguerra@gmail.com>
2018-01-02 17:24:19 -0500

    Complete frontend auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { AuthRoute } from '../util/route_util';
+
 import WelcomeContainer from './welcome/welcome_container';
+import SessionFormContainer from './session/session_form_container';
 
 const App = (props) => (
   <div>
     <header>
       <h1>Lets!!</h1>
-      <WelcomeContainer />
+      <Switch>
+        <Route path="/login" />
+        <Route path="/signup" />
+        <WelcomeContainer />
+      </Switch>
     </header>
+
+    <AuthRoute path="/login" component={ SessionFormContainer } />
+    <AuthRoute path="/signup" component={ SessionFormContainer } />
   </div>
 );
 
--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,5 +1,76 @@
 import React from 'react';
+import {
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
 
 class SessionForm extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  renderErrors() {
+    if (this.props.errors) {
+      return (
+        <ul>
+          {
+            this.props.errors.map((err, id) => {
+              <l key={id}>{err}</l>;
+            })
+          }
+        </ul>
+      )
+    }
+  }
+
+  update(field) {
+    return e => {
+      this.setState({ [field]: e.target.value })
+    };
+  }
+
+  render() {
+    if (this.props.loggedIn) {
+      return <Redirect to="/" />;
+    }
+    const text = (this.props.formType === 'login') ? "Log In" : "Sign Up";
+    const link = (text === "Log In") ? ['signup', "Sign Up"] : ['login', "Log In"];
+    return (
+      <div>
+  <br/>
+  <form onSubmit={this.handleSubmit}>
+    <label>Email:
+      <input
+        type="text"
+        value={this.state.email}
+        onChange={this.update('email')} />
+    </label>
+    <br />
+    <label>Password:
+      <input
+        type="password"
+        value={this.state.password}
+        onChange={this.update('password')} />
+    </label>
+    <button type="submit" >{text}</button>
+  </form>
+  <p>{text}, or <Link to={`/${link[0]}`}>{link[1]}</Link></p>
+  {this.renderErrors()}
+</div>
+    )
+  }
 }
+
+export default withRouter(SessionForm);
