c65cf349e009bbc35ec8ea0621ecd3295c2b11f0
Carib Rivera-Guerra <caribfguerra@gmail.com>
2018-01-06 16:36:55 -0500

    Try to fix precompile error

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module LetterApp
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
